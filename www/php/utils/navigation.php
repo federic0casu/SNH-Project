f2bc46ddcc58a409606e0fa12f775211caa15454
SilverBeamx <molto.falso@gmail.com>
2024-03-02 20:23:08 +0100

    Better redirects

--- www/php/utils/navigation.php
+++ www/php/utils/navigation.php
@@ -5,13 +5,13 @@
 //after being urlencoded.
 function redirect_to_page(string $page_name, string $query=NULL) : void{
     //Build url to the page directory
-    $url = "../../pages/".$page_name.'.html';
+    $url = "/pages/".$page_name.'.html';
     //If query was submitted, append it to the url
     if(!is_null($query)){
         $url = $url.'?'.$query;
     }
     //Redirect to the computed url
-    header("Location: ".$url);
+    header("Location: //".$_SERVER["SERVER_NAME"].$url);
     //Close connection to db if it was open, then exit
     if(isset($conn)){
         $conn->close();
@@ -24,6 +24,11 @@ function redirect_to_page(string $page_name, string $query=NULL) : void{
 function redirect_with_error(string $page_name, string $error_message) : void{
     //Urlencode the error message, and call the redirect function
     redirect_to_page($page_name, "error=".urlencode($error_message));
+}
+
+//Redirect to the index page
+function redirect_to_index() : void{
+    header("Location: //".$_SERVER["SERVER_NAME"]);
 }
 
 ?>
