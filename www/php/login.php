adbb0f7b666f465a34f63df6cef24ffc3742e6d0
SilverBeamx <molto.falso@gmail.com>
2024-03-08 09:56:42 +0100

    Add login functionality

--- www/php/login.php
+++ www/php/login.php
@@ -1,49 +1,72 @@
 <?php
-// Include the database connection
-include_once 'db_connect.php';
+include_once 'utils/config_and_import.php';
 
-if(isset($_POST['username']) and isset($_POST['password'])) {
-    $user = $_POST['username'];
-    $pswd = $_POST['password'];
-
-    // Using prepared statement to prevent SQL injection
-    $sql = $conn->prepare("SELECT * FROM users WHERE users.username = ? LIMIT 1");
-    $sql->bind_param("s", $user);
-
-    // Execute the statement
-    $sql->execute();
-
-    // Get the result
-    $result = $sql->get_result();
-
-    // Close the connection
-    $conn->close();
-
-    // Fetch data from the result
-    if ($row = $result->fetch_assoc()) {
-        if (password_verify($pswd, $row['password'])) {
-            session_start();
-
-            $_SESSION['username'] = array(
-                'username'  => $user,
-                'firstname' => $row['first_name'],
-                'last_name' => $row['last_name'],
-                'logged_in' => 1
-            );
-
-            header("Location: ../index.php");
-        } else
-            // The provided password is wrong
-            header("Location: ../pages/login.html?error=true");
-    } else {
-        // The user provides the wrong username
-        header("Location: ../pages/login.html?error=true");
-    }
-} else {
-    // Close the connection
-    $conn->close();
-
-    header("Location: ../pages/login.html?error=true");
+//Check that the user isn't already logged in
+$user_id = get_logged_user_id();
+if($user_id > 0){
+    redirect_to_index();
 }
+
+//Check that all needed data was supplied and is a string
+check_post_field_array("login", ["username", "password"]);
+
+//Check that the password has the correct format
+$pass_error = check_valid_password($_POST["password"]);
+if(!empty($pass_error)){
+    redirect_with_error("login", $pass_error);
+}
+
+//Get DB instance
+$db = DBManager::get_instance();
+
+//Check if the user is registered
+$query = "SELECT * FROM `users` WHERE `username` = ?";
+$query_rows = $db->exec_query("SELECT", $query, [$_POST["username"]], "s");
+if(count($query_rows) == 0){
+    //TODO: SECURITY log this
+    redirect_with_error("login", "Username does not exist");
+}
+
+//Get user data from query result
+$user = $query_rows[0];
+
+//Check if the user is timed-out
+$query = "SELECT * FROM `wrong_login` WHERE `user_id` = ? AND `created_at` > DATE_SUB(NOW(), INTERVAL 1 MINUTE)";
+$query_rows = $db->exec_query("SELECT", $query, [$user["id"]], "i");
+if(count($query_rows) >= $lockout_threshold){
+    //TODO: SECURITY log this
+    redirect_with_error("login", "Too many recently failed login attempts. Retry after a while.");
+}
+
+//Check if the user is verified
+if($user["is_verified"] == 0){
+    redirect_with_error("login", "User is not verified. Check your mail for the verification link.");
+}
+
+//Check the password
+if(!password_verify($_POST["password"], $user["password"])){
+    //TODO: SECURITY log this
+
+    //Log wrong login attempt
+    $query = "INSERT INTO `wrong_login` (`user_id`) VALUES (?)";
+    $query_result = $db->exec_query("INSERT", $query, [$user["id"]], "i");
+
+    //Redirect user
+    redirect_with_error("login", "Password is not correct.");
+}
+
+//If we arrived here, all checks have succeeded
+//Generate the user login session cookie
+$session_token = bin2hex(random_bytes(32));
+$query = "INSERT INTO `logged_users` (`user_id`, `session_token`, `valid_until`) VALUES ".
+         "(?, ?, DATE_ADD(NOW(), INTERVAL 7 DAY))";
+$query_result = $db->exec_query("INSERT", $query, [$user["id"],$session_token], "is");
+
+//Save the cookie
+setcookie("user_login", $session_token, time() + 7 * 24 * 60 * 60, "/", "", true, true);
+
+//Redirect to home
+redirect_to_index();
+
 ?>
 
