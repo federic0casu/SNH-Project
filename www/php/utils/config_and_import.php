9f183d4fd9bbaff8d72faf9ee253d9e9ff4c19ef
SilverBeamx <molto.falso@gmail.com>
2024-03-08 09:55:45 +0100

    Add bulk import and config php

---
+++ www/php/utils/config_and_import.php
@@ -0,0 +1,12 @@
+<?php
+//UTIL IMPORTS
+include_once 'csrf.php';
+include_once 'db_manager.php';
+include_once 'db_utils.php';
+include_once 'navigation.php';
+include_once 'validation.php';
+
+//CONSTANTS
+$lockout_threshold = 3;
+
+?>
