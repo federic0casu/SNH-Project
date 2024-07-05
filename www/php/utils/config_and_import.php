e4f310a27f736e33ecba16ec090debea30b1facf
federico <federicocasu.mail@gmail.com>
2024-07-05 12:11:45 +0200

    Added send mail function

--- www/php/utils/config_and_import.php
+++ www/php/utils/config_and_import.php
@@ -6,6 +6,7 @@ include_once 'db_utils.php';
 include_once 'logger.php';
 include_once 'navigation.php';
 include_once 'validation.php';
+include_once 'mail_utils.php';
 
 //CONSTANTS
 $lockout_threshold = 3;
---
+++ www/php/utils/mail_utils.php
@@ -0,0 +1,77 @@
+<?php
+// Include the phpmailer loader
+require_once $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
+
+//Import PHPMailer classes into the global namespace
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+include_once 'logger.php';
+
+function sendMail($email, $name, $subject, $message) {
+    //Get Logger instance 
+    $logger = Logger::getInstance();
+    
+    $mail = new PHPMailer(true);
+    $error = "";
+
+    try {
+        //enable verbose debug output (2 for detailed debug output)
+        //$mail->SMTPDebug = 2;
+
+        //using the SMRP protocol to send the email
+        $mail->isSMTP();
+
+        $mail->SMTPAuth = true;
+        $mail->Host     = 'smtp.gmail.com';
+        $mail->Username = 'federicocasu.mail@gmail.com';
+        $mail->Password = '16 chars password (it should be in some .env file)';
+
+        //by setting SMTPSecure to PHPMailer::ENCRYPTION_STARTTLS
+        //we are telling PHPMailer to use TLS encryption method 
+        //when connecting to the SMTP server. This helps ensure
+        //that the communication between our application and the
+        //SMTP server is encrypted.
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+
+        $mail->SMTPAuth = true;
+
+        //TCP port to connect with Gmail SMTP server.
+        $mail->Port = 587;
+
+        //sender information
+        $mail->setFrom('federicocasu.mail@gmail.com', 'bookemporium.com');
+
+        //receiver email address and name
+        $mail->addAddress($email, $name);  
+
+        //by setting IsHTML(true), we inform PHPMailer that the
+        //email will include HTML markup.
+        $mail->IsHTML(true);
+
+        $mail->Subject = $subject;
+        $mail->Body = $message;
+
+        $error = 'OK';
+        // Attempt to send the email
+        if (!$mail->send()) {
+            $error = "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            $logger->warning('[SEND_MAIL] Failed attempt', ['result' => $error, 'to' => $email]);
+        }
+
+    } catch (Exception $e) {
+        $error = "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        $logger->warning('[SEND_MAIL] Failed attempt', ['result' => $error, 'to' => $email]);
+    }
+
+    return $error;
+}
+
+//Example of usage
+//$email = 'f.casu1@studenti.unipi.it';
+//$name = 'Federico Casu';
+//$subject = 'PHPMailer test';
+//$message = 'PHPMailer the awesome Package\nPHPMailer is working fine for sending mail\nThis is a tutorial to guide you on PHPMailer integration';
+//sendMail($mail, $name, $subject, $message);
+?>
