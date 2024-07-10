6677e4111294242a2935a8f9280cc60b828013e7
SilverBeamx <molto.falso@gmail.com>
2024-07-10 13:10:29 +0200

    Fix books.php query

--- www/php/books.php
+++ www/php/books.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $db = DBManager::get_instance();
 
-    $query = "SELECT * FROM `books` WHERE `book_title` LIKE ? OR `book_author` LIKE ? ORDER BY `book_title` LIMIT 9";
+    $query = "SELECT * FROM `books` WHERE `book_title` LIKE ? AND `book_author` LIKE ? ORDER BY `book_title` LIMIT 9";
     $query_rows = $db->exec_query("SELECT", $query, ["%".$_POST["search_query_title"]."%",
                                                      "%".$_POST["search_query_author"]."%"], "ss");
 
